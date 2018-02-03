97ff6f89b8d6f7cab66211f9d6993dbd4ca348de
Fabien Crespel <fabien@crespel.net>
2018-02-03 22:39:11 +0000

    HoloNet: update for RPMod 0.5.1

--- themes/jediholonet/include/AccountInfo.inc.php
+++ themes/jediholonet/include/AccountInfo.inc.php
@@ -9,16 +9,16 @@ function printAccountInfo($username) {
 		$classParser = new ClassParser(TEMPLATEPATH . '/include/classes.dat');
 		$fpcParser = new FPCParser(TEMPLATEPATH . '/include/forcecost.dat');
 		$forcePowerCost = $fpcParser->getFPC();
-		
+
 		// Get the Account data
 		$response = $RPModClient->GetAccountData($username);
-		
+
 		// Create the Account array from the response
 		$account = array();
 		foreach ($response as $currentpair) {
 			$account[$currentpair->key] = $currentpair->value;
 		}
-		
+
 		// Complete account data with default values if they are missing
 		if (!isset($account['xp']))
 			$account['xp'] = 0;
@@ -34,17 +34,17 @@ function printAccountInfo($username) {
 			$account['forceBonds'] = '';
 		if (!isset($account['forceTemplateMax']))
 			$account['forceTemplateMax'] = '';
-		
+
 		// Determine the reference Force Template
 		if ($GLOBALS['rpmod_config']['lockTemplate']) {
 			$refTemplate = $account['forceTemplateMax'];
 		} else {
 			$refTemplate = $account['forceTemplate'];
 		}
-		
+
 		// Account Info beginning
 		echo '<p>';
-		
+
 		// Class
 		echo '<strong>Class:</strong> ';
 		if (empty($account['className'])) {
@@ -56,27 +56,25 @@ function printAccountInfo($username) {
 			}
 		}
 		echo "<br />\n";
-		
+
 		// XP and points remaining (if available)
 		echo '<strong>XP:</strong> ';
-		if ($refTemplate) {
-			if (strlen($refTemplate) == NUM_FORCE_POWERS) {
-				$usedPoints = 0;
-				for ($i = 0; $i < NUM_FORCE_POWERS; $i++) {
-					for ($j = 0; $j < $refTemplate[$i]; $j++) {
-						$usedPoints += $forcePowerCost[$i][$j];
+		if ($refTemplate && strlen($refTemplate) >= NUM_FORCE_POWERS) {
+			$usedPoints = 0;
+			for ($i = 0; $i < NUM_FORCE_POWERS_EXT; $i++) {
+				if (isset($refTemplate[$i])) {
+					for ($powerLevel = 0; $powerLevel < $refTemplate[$i]; $powerLevel++) {
+						$usedPoints += $forcePowerCost[$i][$powerLevel];
 					}
 				}
-				$unusedPoints = intval($account['xp']) - $usedPoints;
-				echo intval($account['xp']) . ' (' . $usedPoints . ' used, ' . $unusedPoints . ' remaining)';
-			} else {
-				echo intval($account['xp']);
 			}
+			$unusedPoints = intval($account['xp']) - $usedPoints;
+			echo intval($account['xp']) . ' (' . $usedPoints . ' used, ' . $unusedPoints . ' remaining)';
 		} else {
 			echo intval($account['xp']);
 		}
 		echo "<br />\n";
-		
+
 		// Level (Class Parser needed)
 		if (is_object($classParser)) {
 			echo '<strong>Level:</strong> ';
@@ -85,30 +83,30 @@ function printAccountInfo($username) {
 				$curLevel = $classParser->getLevelFromXP($account['className'], $account['xp']);
 				$curLevelNum = $classParser->getLevelNumFromXP($account['className'], $account['xp']);
 				$nextLevel = $classParser->getLevelFromNum($account['className'], $curLevelNum+1);
-				
+
 				// Current level
 				echo $curLevelNum;
-				
+
 				// Next level (if any)
 				echo "<br />\n";
 				echo '<strong>Next level:</strong> ';
-				
+
 				if (isset($nextLevel) && isset($nextLevel['minXP'])) {
 					echo 'level ' . ($curLevelNum+1) . ' with ' . $nextLevel['minXP'] . ' XP (+' . ($nextLevel['minXP'] - $account['xp']) . ' XP)';
 				} else {
 					echo 'none';
 				}
-				
+
 			} else {
 				echo '<span class="error">none</span>';
 			}
 			echo "<br />\n";
 		}
-		
+
 		// Rank and AdminRank
 		echo '<strong>Rank:</strong> ' . $GLOBALS['rpmod_config']['ranks'][intval($account['rank'])] . "<br />\n";
 		echo '<strong>AdminRank:</strong> ' . $GLOBALS['rpmod_config']['adminRanks'][intval($account['adminRank'])] . "<br />\n";
-		
+
 		// Model Scale
 		echo '<strong>Model scale:</strong> ';
 		if ($account['modelScale']) {
@@ -117,7 +115,7 @@ function printAccountInfo($username) {
 			echo 'default';
 		}
 		echo "<br />\n";
-		
+
 		// Force bonds
 		echo '<strong>Force bonds:</strong> ';
 		if ($account['forceBonds']) {
@@ -131,76 +129,71 @@ function printAccountInfo($username) {
 			echo 'none';
 		}
 		echo "<br />\n";
-		
+
 		// Force regen time (level & class parser needed)
 		if (isset($curLevel) && isset($curLevel['forceRegenTime'])) {
 			echo '<strong>Force regeneration time:</strong> ' . $curLevel['forceRegenTime'] . ' ms';
 		}
-		
+
 		echo "</p>\n";
-		
+
 		// Last Force Template
 		echo "<p><strong>Force Template:</strong></p>\n";
 		if ($refTemplate) {
-			if (strlen($refTemplate) != 18) {
-				echo "<p class=\"error\">Invalid reference template in database.</p>\n";
-			} else {
-				echo "<table class=\"ft_table\">\n";
-				for ($i = 0; $i < count($GLOBALS['rpmod_config']['orderedPowers']); $i++) {
-					if ($GLOBALS['rpmod_config']['orderedPowers'][$i] == -1) {
-						// Blank line
-						echo "  <tr><td colspan=\"7\"><p></p></td></tr>\n";
-					} else {
-						$curPower = $GLOBALS['rpmod_config']['orderedPowers'][$i];
-						$curPoints = 0;
-						$totalPoints = 0;
-						$curUnusedPoints = $unusedPoints;
-						
-						echo "  <tr>\n";
-						echo '    <th class="ft_title">' . $GLOBALS['rpmod_config']['forcePowerNames'][$curPower] . ":</th>\n";
-						
-						for ($j = 0; $j < 5; $j++) {
-							if ($GLOBALS['rpmod_config']['restrictForce'] && isset($curLevel) && isset($curLevel['forceTemplateMax']) && $j >= $curLevel['forceTemplateMax'][$curPower]) {
-								// Class-restricted
-								echo '    <td class="ft_disabled">[' . $forcePowerCost[$curPower][$j] . "]</td>\n";
-							} else if ($GLOBALS['rpmod_config']['lockTemplate'] && $j >= $account['forceTemplateMax'][$curPower]) {
-								// New power
-								if ($forcePowerCost[$curPower][$j] <= $curUnusedPoints) {
-									// We can buy this one
-									echo '    <td class="ft_new">[' . $forcePowerCost[$curPower][$j] . "]</td>\n";
-									$curUnusedPoints -= $forcePowerCost[$curPower][$j];
-								} else {
-									// Unavailable for the moment
-									echo '    <td class="ft_unavailable">[' . $forcePowerCost[$curPower][$j] . "]</td>\n";
-								}
+			echo "<table class=\"ft_table\">\n";
+			foreach ($GLOBALS['rpmod_config']['orderedPowers'] as $power) {
+				if ($power == -1) {
+					// Blank line
+					echo "  <tr><td colspan=\"7\"><p></p></td></tr>\n";
+				} else {
+					$curPoints = 0;
+					$totalPoints = 0;
+					$curUnusedPoints = $unusedPoints;
+
+					echo "  <tr>\n";
+					echo '    <th class="ft_title">' . $GLOBALS['rpmod_config']['forcePowerNames'][$power] . ":</th>\n";
+
+					for ($powerLevel = 0; $powerLevel < 5; $powerLevel++) {
+						if ($GLOBALS['rpmod_config']['restrictForce'] && isset($curLevel) && isset($curLevel['forceTemplateMax']) && (!isset($curLevel['forceTemplateMax'][$power]) || $powerLevel >= $curLevel['forceTemplateMax'][$power])) {
+							// Class-restricted
+							echo '    <td class="ft_disabled">[' . $forcePowerCost[$power][$powerLevel] . "]</td>\n";
+						} else if ($GLOBALS['rpmod_config']['lockTemplate'] && isset($account['forceTemplateMax'][$power]) && $powerLevel >= $account['forceTemplateMax'][$power]) {
+							// New power
+							if ($forcePowerCost[$power][$powerLevel] <= $curUnusedPoints) {
+								// We can buy this one
+								echo '    <td class="ft_new">[' . $forcePowerCost[$power][$powerLevel] . "]</td>\n";
+								$curUnusedPoints -= $forcePowerCost[$power][$powerLevel];
 							} else {
-								// Known power
-								if ($j < $account['forceTemplate'][$curPower]) {
-									// Selected one
-									echo '    <td class="ft_on">[' . $forcePowerCost[$curPower][$j] . "]</td>\n";
-									$curPoints += $forcePowerCost[$curPower][$j];
-								} else {
-									// Not selected but available
-									echo '    <td class="ft_off">[' . $forcePowerCost[$curPower][$j] . "]</td>\n";
-									if ($GLOBALS['rpmod_config']['lockTemplate']) {
-										$curPoints += $forcePowerCost[$curPower][$j];
-									}
+								// Unavailable for the moment
+								echo '    <td class="ft_unavailable">[' . $forcePowerCost[$power][$powerLevel] . "]</td>\n";
+							}
+						} else {
+							// Known power
+							if (isset($account['forceTemplate'][$power]) && $powerLevel < $account['forceTemplate'][$power]) {
+								// Selected one
+								echo '    <td class="ft_on">[' . $forcePowerCost[$power][$powerLevel] . "]</td>\n";
+								$curPoints += $forcePowerCost[$power][$powerLevel];
+							} else {
+								// Not selected but available
+								echo '    <td class="ft_off">[' . $forcePowerCost[$power][$powerLevel] . "]</td>\n";
+								if ($GLOBALS['rpmod_config']['lockTemplate']) {
+									$curPoints += $forcePowerCost[$power][$powerLevel];
 								}
 							}
-							$totalPoints += $forcePowerCost[$curPower][$j];
 						}
-						
-						// Used points / total points for this power
-						echo '    <td class="ft_points">(' . $curPoints . ' / ' . $totalPoints . ")</td>\n";
-						echo "  </tr>\n";
+						$totalPoints += $forcePowerCost[$power][$powerLevel];
 					}
+
+					// Used points / total points for this power
+					echo '    <td class="ft_points">(' . $curPoints . ' / ' . $totalPoints . ")</td>\n";
+					echo "  </tr>\n";
 				}
-				echo "</table>\n";
 			}
+			echo "</table>\n";
 		} else {
 			echo "<p>None stored.</p>\n";
 		}
-		
+
 	} catch (Exception $e) {
 		echo "<p><strong>ERROR:</strong> {$e->getMessage()}</p>\n";
 	}
